70ec0042ae9872267201e92f7877942b1607915b
developer-p <float@kakao.com>
2022-02-27 15:54:01 +0900

    [Bronze IV] Title: 10부제, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/10797.10부제/10부제.swift
+++ 백준/Bronze/10797.10부제/10부제.swift
@@ -1,12 +1,4 @@
-// MARK: - 10797번
+// MARK: - 10797번(2)
 let input = Int(readLine()!)!
-let numArray = readLine()!.split(separator: " ").map{Int(String($0))!}
-var count: Int = 0
-
-for num in numArray {
-    if input == num {
-        count += 1
-    }
-}
-
-print(count)
+let answer = readLine()!.split(separator: " ").filter{Int($0) == input}
+print(answer.count)
