fc300621737ca01065129c363362e4cd39509390
developer-p <float@kakao.com>
2022-02-27 15:40:16 +0900

    [Bronze IV] Title: 사분면 고르기, Time: 8 ms, Memory: 62224 KB -BaekjoonHub

---
+++ 백준/Bronze/14681.사분면 고르기/사분면 고르기.swift
@@ -0,0 +1,16 @@
+// 백준 - 14681번
+let x = Int(readLine()!)!
+let y = Int(readLine()!)!
+
+if x > 0 && y > 0 {
+    print("1")
+}
+else if x < 0 && y > 0 {
+    print("2")
+}
+else if x < 0 && y < 0 {
+    print("3")
+}
+else {
+    print("4")
+}
