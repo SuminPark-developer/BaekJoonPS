95d873e72c86e5308cb42f0a054dba70eff0650f
developer-p <float@kakao.com>
2022-02-27 15:34:17 +0900

    [Bronze IV] Title: 시험 성적, Time: 8 ms, Memory: 62224 KB -BaekjoonHub

---
+++ 백준/Bronze/9498.시험 성적/시험 성적.swift
@@ -0,0 +1,14 @@
+// 백준 - 9498번(2)
+let score = Int(readLine()!)!
+switch score {
+case 90...100:
+    print("A")
+case 80...89:
+    print("B")
+case 70...79:
+    print("C")
+case 60...69:
+    print("D")
+default:
+    print("F")
+}
