134a91589ab6999c9cc752bfc24d9a2f60e17c32
developer-p <float@kakao.com>
2022-03-07 16:36:47 +0900

    [Bronze II] Title: 부녀회장이 될테야, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.swift
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.swift
@@ -1,17 +1,28 @@
-// MARK: - 2775번(1) - 재귀함수
+// MARK: - 2775번(2) - 2차원배열
 let T = Int(readLine()!)!
-
-func getCitizen(_ k: Int, _ n: Int) -> Int {
-    if k == 0 || n == 1 {
-        return n
-    }
-    
-    return getCitizen(k - 1, n) + getCitizen(k, n - 1) // (k-1층의 n호), (k층의 n-1호)
-}
 
 for _ in 0..<T {
     let k = Int(readLine()!)!
     let n = Int(readLine()!)!
     
-    print(getCitizen(k, n))
+    var apart: [[Int]] = Array(repeating: Array(repeating: 0, count: n + 1), count: k + 1)
+    
+    for i in 0...k { // 각 층을 돌면서,
+        for j in 0...n { // 각 호를 돌면서,
+            if i == 0 { // 0층일 땐,
+                apart[i][j] = j + 1
+            }
+            else { // 다른 층일 땐,
+                if j == 0 { // 1호일 땐,
+                    apart[i][j] = 1
+                }
+                else { // 2호이상일땐,
+                    apart[i][j] = apart[i-1][j] + apart[i][j-1]
+                }
+            }
+            
+        }
+        
+    }
+    print(apart[k][n-1])
 }
