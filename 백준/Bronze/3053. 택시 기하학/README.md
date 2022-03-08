a052f3965df3ce5ac6955dae5e65d2fc355375f9
developer-p <float@kakao.com>
2022-03-08 12:19:07 +0900

    [Bronze III] Title: 택시 기하학, Time: 16 ms, Memory: 79160 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] 택시 기하학 - 3053 
+
+[문제 링크](https://www.acmicpc.net/problem/3053) 
+
+### 성능 요약
+
+메모리: 79160 KB, 시간: 16 ms
+
+### 분류
+
+기하학(geometry), 수학(math)
+
---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.swift
@@ -0,0 +1,8 @@
+// 백준 - 3053번(2r^2)
+import Foundation
+let R = Double(readLine()!)!
+
+let result: String = String(format: "%.6f", Double.pi * R * R)
+print(result)
+print(2 * R * R)
+
