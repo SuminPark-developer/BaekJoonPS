fb3af850fcddfe14dd05445534be448ffb6b1ccd
developer-p <float@kakao.com>
2022-03-14 17:28:06 +0900

    [Silver I] Title: 절댓값 힙, Time: 76 ms, Memory: 71280 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.swift
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.swift
@@ -1,128 +1,106 @@
-// MARK: - 11286번 (참고 : https://www.acmicpc.net/source/37235051)
-import Foundation
+// MARK: - 11286번(풀이 2 - idea: 튜플을 넣어서 비교)
+struct MinHeap<T: Comparable> {
+    var heap: [(T, T)] = []
 
-struct absMinHeap<T: Comparable> {
-    var heap: [Int] = []
-    
-    var comparer: (Int, Int) -> Bool = {(a, b) in
-        if abs(a) == abs(b) {
-            return a > b
-        }
-        else {
-            return abs(a) > abs(b)
-        }
-    }
-    
     var isEmpty: Bool {
         return heap.count <= 1 ? true : false
     }
-    
+
     init() {}
-    init(_ element: Int) {
-        heap.append(element) // 0번 index채우기 용
-        heap.append(element) // 실제 Root Node 채움.
+    init(_ element: (T, T)) {
+        heap.append(element)
+        heap.append(element)
     }
-    
-    mutating func insert(_ element: Int) {
+
+    mutating func insert(_ element: (T, T)) {
         if heap.isEmpty {
             heap.append(element)
             heap.append(element)
             return
         }
         heap.append(element)
-        
+
+
         func isMoveUp(_ insertIndex: Int) -> Bool {
-            if insertIndex <= 1 { // Root Node일 때,
+            if insertIndex <= 1 { // root node
                 return false
             }
             let parentIndex = insertIndex / 2
-//            return heap[insertIndex] < heap[parentIndex] ? true : false
-            return comparer(heap[parentIndex], heap[insertIndex])
+            return heap[insertIndex] < heap[parentIndex] ? true : false
         }
-        
-        var insertIndex = heap.count - 1
+
+        var insertIndex: Int = heap.count - 1
         while isMoveUp(insertIndex) {
             let parentIndex = insertIndex / 2
             heap.swapAt(insertIndex, parentIndex)
             insertIndex = parentIndex
         }
     }
-    
-    enum moveDownStatus { case left, right, none }
-    
-    mutating func pop() -> Int? {
+
+    enum moveDownStatus {case left, right, none}
+
+    mutating func pop() -> (T, T)? {
         if heap.count <= 1 {
             return nil
         }
         let returnData = heap[1]
-        heap.swapAt(1, heap.count - 1)
+        heap.swapAt(1, heap.count-1)
         heap.removeLast()
-        
+
         func moveDown(_ poppedIndex: Int) -> moveDownStatus {
             let leftChildIndex = poppedIndex * 2
             let rightChildIndex = leftChildIndex + 1
-            
-            // case1. 모든(왼쪽) 자식 노드가 없는 경우(완전이진트리는 왼쪽부터 채워지므로)
-            if leftChildIndex >= heap.count {
+
+            if leftChildIndex >= heap.count { // 자식 노드가 없을 경우,
                 return .none
             }
-            
-            // case2. 왼쪽 자식 노드만 있는 경우
-            if rightChildIndex >= heap.count {
-//                return heap[leftChildIndex] < heap[poppedIndex] ? .left : .none
-                return comparer(heap[poppedIndex], heap[leftChildIndex]) ? .left : .none
+
+            if rightChildIndex >= heap.count { // 왼쪽 자식 노드밖에 없을 경우,
+                return heap[leftChildIndex] < heap[poppedIndex] ? .left : .none
             }
-            
-            // case3. 왼쪽&오른쪽 자식 노드 모두 있는 경우
-            // case3-1. 자식들이 자신보다 모두 큰 경우
-//            if (heap[leftChildIndex] > heap[poppedIndex]) && (heap[rightChildIndex] > heap[poppedIndex]) {
-                if (comparer(heap[leftChildIndex], heap[poppedIndex])) && (comparer(heap[rightChildIndex], heap[poppedIndex])) {
+
+            if (heap[poppedIndex] < heap[leftChildIndex]) && (heap[poppedIndex] < heap[rightChildIndex]) { // 자식 노드보다 내 노드가 더 큰 경우,
                 return .none
             }
-            
-            // case3-2. 자식들이 자신보다 모두 작은 경우(왼쪽과 오른쪽 자식 중, 더 작은 자식을 선별)
-//            if (heap[leftChildIndex] < heap[poppedIndex]) && (heap[rightChildIndex] < heap[poppedIndex]) {
-                if (comparer(heap[poppedIndex], heap[leftChildIndex])) && (comparer(heap[poppedIndex], heap[rightChildIndex])) {
-//                return heap[leftChildIndex] < heap[rightChildIndex] ? .left : .right
-                return comparer(heap[rightChildIndex], heap[leftChildIndex]) ? .left : .right
+
+            if (heap[poppedIndex] > heap[leftChildIndex]) && (heap[poppedIndex] > heap[rightChildIndex]) { // 자식 노드들이 내 노드보다 둘 다 작은 경우,
+                return heap[leftChildIndex] < heap[rightChildIndex] ? .left : .right
             }
-            
-            // case3-3. 왼쪽과 오른쪽 자식 중, 한 자식만 자신보다 작은 경우
-//            return heap[leftChildIndex] < heap[poppedIndex] ? .left : .right
-            return comparer(heap[poppedIndex], heap[leftChildIndex]) ? .left : .right
-            
+            // 둘 중 하나의 자식 노드만 작은 경우,
+            return heap[leftChildIndex] < heap[poppedIndex] ? .left : .right
         }
-        
-        var poppedIndex = 1
+
+        var poppedIndex: Int = 1
         while true {
             switch moveDown(poppedIndex) {
             case .none:
                 return returnData
             case .left:
                 let leftChildIndex = poppedIndex * 2
-                heap.swapAt(poppedIndex, leftChildIndex)
+                heap.swapAt(leftChildIndex, poppedIndex)
                 poppedIndex = leftChildIndex
             case .right:
-                let rightChildIndex = (poppedIndex * 2) + 1
-                heap.swapAt(poppedIndex, rightChildIndex)
+                let rightChildIndex = poppedIndex * 2 + 1
+                heap.swapAt(rightChildIndex, poppedIndex)
                 poppedIndex = rightChildIndex
             }
         }
+
     }
+
 }
 
 let N = Int(readLine()!)!
-var myAbsHeap: absMinHeap<Int> = absMinHeap()
+var myMinHeap: MinHeap<Int> = MinHeap()
 
 for _ in 0..<N {
     let input = Int(readLine()!)!
     
     if input == 0 {
-        let answer = myAbsHeap.pop()
-        answer == nil ? print("0") : print(answer!)
+        let answer = myMinHeap.pop()
+        answer == nil ? print("0") : print(answer!.1) // 튜플의 뒷부분(원본값) 출력.
     }
     else {
-        myAbsHeap.insert(input)
+        myMinHeap.insert((abs(input), input))
     }
-    
 }
