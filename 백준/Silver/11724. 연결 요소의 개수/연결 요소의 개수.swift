20d7ee5d7c8163978a967578ac7d5816c40b3a09
developer-p <float@kakao.com>
2022-03-19 16:15:49 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 608 ms, Memory: 70168 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
@@ -1,8 +1,8 @@
-// MARK: - 11724번(DFS)
+// MARK: - 11724번(BFS)
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (N, M) = (input[0], input[1])
 var adj = Array(repeating: Array(repeating: false, count: N + 1), count: N + 1)
-var check = Array(repeating: false, count: N + 1) // 방문체크
+var check = Array(repeating: false, count: N + 1) // 방문 체크
 var answer: Int = 0
 
 for _ in 0..<M {
@@ -12,11 +12,57 @@ for _ in 0..<M {
     adj[v][u] = true
 }
 
-func dfs(_ index: Int) {
-    for next in 1...N {
-        if (index != next) && (adj[index][next] == true) && (check[next] == false) { // 자기자신X, 연결되어 있고, 방문한 적 없으면,
-            check[next] = true
-            dfs(next)
+class Dequeue<T> {
+    var enQueue: [T]
+    var deQueue: [T] = []
+    
+    var count: Int {
+        return enQueue.count + deQueue.count
+    }
+    
+    var isEmpty: Bool {
+        return enQueue.isEmpty && deQueue.isEmpty
+    }
+    
+    init(_ queue: [T]) {
+        enQueue = queue
+    }
+    
+    func pushLast(_ element: T) {
+        enQueue.append(element)
+    }
+    
+    func pushFirst(_ element: T) {
+        deQueue.append(element)
+    }
+    
+    func popLast() -> T {
+        if enQueue.isEmpty {
+            enQueue = deQueue.reversed()
+            deQueue.removeAll()
+        }
+        return enQueue.popLast()!
+    }
+    
+    func popFirst() -> T {
+        if deQueue.isEmpty {
+            deQueue = enQueue.reversed()
+            enQueue.removeAll()
+        }
+        return deQueue.popLast()!
+    }
+}
+
+func bfs(_ startIndex: Int) {
+    var q = Dequeue([startIndex])
+    
+    while !q.isEmpty {
+        let now = q.popFirst()
+        for next in 1...N {
+            if (adj[now][next] == true) && (check[next] == false) { // 연결되어 있고, 방문한 적 없으면,
+                check[next] = true
+                q.pushLast(next)
+            }
         }
     }
     
@@ -26,9 +72,8 @@ for i in 1...N {
     if check[i] == false {
         check[i] = true
         answer += 1
-        dfs(i)
+        bfs(i)
     }
-    
 }
 
 print(answer)
