5ab2a5a6fcbfb81a7b84fde2e34cc272aef20a16
developer-p <float@kakao.com>
2022-03-22 13:26:08 +0900

    [Silver III] Title: 스택 수열, Time: 128 ms, Memory: 76572 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.swift
+++ 백준/Silver/1874. 스택 수열/스택 수열.swift
@@ -1,33 +1,31 @@
-// MARK: - 1874번
+// MARK: - 1874번(풀이2)
 let N = Int(readLine()!)!
-
-var stack: [Int] = []
-var op: [String] = []
-var num: Int = 1
+var numArray: [Int] = []
 for _ in 0..<N {
-    let input = Int(readLine()!)!
-    
-    while num <= input {
-        stack.append(num)
-        op.append("+")
-        num += 1
-    }
-    
-    if stack.last! == input {
-        stack.removeLast()
-        op.append("-")
-    }
-    else {
-        op.append("NO")
-        break
-    }
+    numArray.append(Int(readLine()!)!)
 }
 
-if op.contains("NO") {
+var stack: [Int] = []
+var operatorArray: [String] = []
+var index: Int = 0
+
+for i in 1...N {
+    stack.append(i)
+    operatorArray.append("+")
+    
+    while !stack.isEmpty && stack.last! == numArray[index] { // stack이 비어있지 않고, 스택의 마지막이 pop을 해야하는 정수면,
+        stack.removeLast()
+        index += 1
+        operatorArray.append("-")
+    }
+    
+}
+
+if !stack.isEmpty {
     print("NO")
 }
 else {
-    for o in op { // operator 출력.
-        print(o)
+    for op in operatorArray {
+        print(op)
     }
 }
