eef814326aad0249ffefee0cc8a0b7d5e16ff66a
developer-p <float@kakao.com>
2022-04-05 16:02:04 +0900

    [Silver III] Title: 예산, Time: 12 ms, Memory: 70020 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.swift
+++ 백준/Silver/2512. 예산/예산.swift
@@ -4,28 +4,28 @@ let moneyArray = readLine()!.split(separator: " ").map{Int(String($0))!}
 let M = Int(readLine()!)!
 var answerMax: Int = 0
 
-if moneyArray.reduce(0, +) <= M { // 모든 요청이 배정될 수 있는 경우,
-    answerMax = moneyArray.max()!
-}
-else { // 모든 요청이 배정될 수 없는 경우,
-    var start: Int = 0
-    var end: Int = moneyArray.max()!
-    var mid = (start + end) / 2
-    
-    while start <= end {
-        var sum: Int = 0
-        mid = (start + end) / 2
-        for money in moneyArray {
-            sum += money > mid ? mid : money // money가 기준(중간값)보다 크면 기준을 더하고, 기준보다 작거나 같으면 money를 더함.
-        }
-        if sum > M {
-            end = mid - 1
-        }
-        else {
-            start = mid + 1
-        }
+var low = 0
+var high = moneyArray.max()!
+var middle = (low + high) / 2
+
+func isPossible(_ middle: Int) -> Bool {
+    var sum: Int = 0
+    for money in moneyArray {
+        sum += min(money, middle)
     }
-    answerMax = end
+    return sum <= M
 }
 
-print(answerMax) // 배정된 예산들 중 최댓값
+while low <= high { // Parametric Search & Binary Search
+    if isPossible(middle) { // 기준예산(middle)으로 충당이 가능하면,
+        low = middle + 1 // 기준예산보다 위로 다시 체크.
+        answerMax = middle
+    }
+    else { // 기준예산(middle)으로 충당이 불가능하면,
+        high = middle - 1 // 기준예산보다 밑으로 다시 체크.
+    }
+    
+    middle = (low + high) / 2
+}
+
+print(answerMax)
