2358fd72cb2214ca482672e08de1db1caa4a4b95
developer-p <float@kakao.com>
2022-04-15 17:03:21 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.swift
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.swift
@@ -1,16 +1,13 @@
-// MARK: - 10870번(DP - 재귀)
+// MARK: - 10870번(DP - 반복문)
 let n = Int(readLine()!)!
-
 var dp = Array(repeating: -1, count: 21)
 dp[0] = 0
 dp[1] = 1
 
-func fibo(_ N: Int) -> Int {
-    if dp[N] == -1 {
-        dp[N] = fibo(N - 2) + fibo(N - 1)
+if n >= 2 {
+    for i in 2...n {
+        dp[i] = dp[i - 1] + dp[i - 2]
     }
-    
-    return dp[N]
 }
 
-print(fibo(n))
+print(dp[n])
