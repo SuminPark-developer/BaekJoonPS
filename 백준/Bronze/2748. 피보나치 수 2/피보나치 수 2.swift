82e93341213bfc295e7152e7d42c220f7b4906f5
developer-p <float@kakao.com>
2022-04-15 21:52:24 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.swift
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.swift
@@ -1,12 +1,12 @@
-// MARK: - 2748번(DP - 바텀업)
+// MARK: - 2748번(DP - 반복문)
 let n = Int(readLine()!)!
-var cache = Array(repeating: -1, count: n + 1)
+var cache = Array(repeating: -1, count: 91)
 cache[0] = 0
 cache[1] = 1
 
 if n >= 2 {
     for i in 2...n {
-        cache[i] = cache[i - 1] + cache[i - 2]
+        cache[i] = cache[i - 2] + cache[i - 1]
     }
 }
 print(cache[n])
