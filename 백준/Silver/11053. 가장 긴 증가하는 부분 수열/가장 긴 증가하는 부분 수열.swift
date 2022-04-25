ab6200575448bcf873651fbdb8c19cd5fe9321e5
developer-p <float@kakao.com>
2022-04-25 15:54:34 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
@@ -1,15 +1,18 @@
-// MARK: - 11053번(DP - 반복문)
+// MARK: - 11053번 다시
 let N = Int(readLine()!)!
-let aArray = readLine()!.split(separator: " ").map{Int(String($0))!}
-var dp = Array(repeating: 1, count: N)
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var dp = Array(repeating: 0, count: N)
+dp[0] = 1
 
 for i in 1..<N {
-    
+    let standard = arr[i]
+    var maxValue = 0
     for j in 0..<i {
-        if aArray[i] > aArray[j] {
-            dp[i] = max(dp[i], dp[j] + 1)
+        if arr[j] < standard { // 기준보다 작지만,
+            maxValue = max(maxValue, dp[j]) // 그 중 제일 큰 dp값에
         }
     }
+    dp[i] = maxValue + 1 // 1을 더한게 dp[n]값.
 }
 
 print(dp.max()!)
