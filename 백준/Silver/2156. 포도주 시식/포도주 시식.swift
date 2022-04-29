7ba1e694ffb0ed25c067b305be4d8eb0703aaf95
developer-p <float@kakao.com>
2022-04-29 22:29:17 +0900

    [Silver I] Title: 포도주 시식, Time: 12 ms, Memory: 69560 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.swift
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.swift
@@ -1,22 +1,29 @@
-// MARK: - 2156번(DP - 반복문)
+// MARK: - 2156번(DP - 재귀함수)
 let n = Int(readLine()!)!
 var arr = [0] + Array(repeating: 0, count: 10000) // 0번째는 무시.
-var dp = [0] + Array(repeating: 0, count: 10000) // 0번째는 무시.
+var dp = [-1] + Array(repeating: -1, count: 10000) // 0번째는 무시.
 
 for i in 1...n {
     arr[i] = Int(readLine()!)!
 }
 
-dp[1] = arr[1]
-dp[2] = arr[1] + arr[2]
-dp[3] = max(arr[1] + arr[2], arr[1] + arr[3], arr[2] + arr[3]) // 인덱스 1 + 2, 1 + 3, 2 + 3
-
-if n >= 4 {
-    for i in 4...n {
-        let case1 = arr[i] + arr[i - 1] + dp[i - 3] // 현재잔 + 직전잔 + i-3위치의 최대값
-        let case2 = arr[i] + dp[i - 2] // 현재잔 + i-2위치의 최대값
-        let case3 = dp[i - 1] // 현재잔X -> i-1위치의 최대값
-        dp[i] = max(case1, case2, case3)
+func topDown(_ N: Int) -> Int {
+    if dp[N] != -1 {
+        return dp[N]
     }
+    if N <= 3 {
+        dp[1] = arr[1]
+        dp[2] = arr[1] + arr[2]
+        dp[3] = max(arr[1] + arr[2], arr[1] + arr[3], arr[2] + arr[3]) // 인덱스 1 + 2, 1 + 3, 2 + 3
+    }
+    else {
+        let case1 = arr[N] + arr[N - 1] + topDown(N - 3) // 현재잔 + 직전잔 + N-3위치의 최대값
+        let case2 = arr[N] + topDown(N - 2) // 현재잔 + N-2위치의 최대값
+        let case3 = topDown(N - 1) // 현재잔X -> N-1위치의 최대값
+        dp[N] = max(case1, case2, case3)
+    }
+    return dp[N]
 }
-print(dp.max()!)
+
+print(topDown(n))
+
