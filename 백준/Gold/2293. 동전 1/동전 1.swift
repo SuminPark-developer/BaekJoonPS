483de6fa1b159ece5f11309d8b4cb1df883888fa
developer-p <float@kakao.com>
2022-05-04 23:07:19 +0900

    [Gold V] Title: 동전 1, Time: 32 ms, Memory: 79676 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.swift
@@ -0,0 +1,25 @@
+// MARK: - 2293번(DP - 반복문)
+import Foundation
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+let (n, k) = (input[0], input[1])
+var coins: [Int] = []
+
+for _ in 0..<n {
+    coins.append(Int(readLine()!)!)
+}
+var dp: [Int] = [1] + Array(repeating: 0, count: k) // dp[0] = 1
+
+for coin in coins {
+    if coin > k { // 동전의 가치가 합k보다 클 때, 에러를 방지하기 위해.
+        continue
+    }
+    
+    for i in coin...k {
+        dp[i] = dp[i] + dp[i - coin]
+        if dp[i] >= Int(pow(2.0, 31.0)) { // 경우의 수가 2^31을 벗어나는 경우, 에러를 방지하기 위해.
+            dp[i] = 0
+        }
+    }
+}
+
+print(dp[k])
