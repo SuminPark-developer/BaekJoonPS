609f22d6ff15f9912fe4d2d54c0835e8eb635b96
developer-p <float@kakao.com>
2022-05-16 15:50:41 +0900

    [Gold V] Title: 합분해, Time: 12 ms, Memory: 69368 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.swift
+++ 백준/Gold/2225. 합분해/합분해.swift
@@ -1,20 +1,19 @@
-// MARK: - 2225번(DP - 반복문(1))
+// MARK: - 2225번(DP - 반복문(2) : 시간 개선)
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (N, K) = (input[0], input[1])
 
 var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: N + 1), count: K + 1)
 
-for i in 0...N {
-    dp[1][i] = 1
+for i in 1...K {
+    dp[i][0] = 1
 }
 
 for i in 1...K {
-    for j in 0...N {
-        for l in 0...j {
-            dp[i][j] += dp[i - 1][j - l]
-            dp[i][j] %= 1000000000
-        }
+    for j in 1...N {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        dp[i][j] %= 1000000000
     }
 }
 
 print(dp[K][N])
+
