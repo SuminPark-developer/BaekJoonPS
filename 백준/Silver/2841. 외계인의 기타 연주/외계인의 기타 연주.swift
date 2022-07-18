0479315d99d28c20f84e7db14e910555c9cb503c
developer-p <float@kakao.com>
2022-07-18 23:27:13 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 644 ms, Memory: 73016 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.swift
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.swift
@@ -9,20 +9,20 @@ for _ in 0..<N {
     let (line, fret) = (input[0], input[1])
     
     while true {
-        if stacks[line].isEmpty {
-            stacks[line].append(fret)
+        if stacks[line].isEmpty { // 그 줄의 스택이 비었으면,
+            stacks[line].append(fret) // 넣고 끝.
             count += 1
             break
         }
         
-        if stacks[line].last! > fret {
+        if stacks[line].last! > fret { // 새로 들어온 프렛이 더 작으면, 그 줄의 스택에서 하나씩 빼냄.
             stacks[line].removeLast()
             count += 1
         }
-        else if stacks[line].last! == fret {
+        else if stacks[line].last! == fret { // 같으면, 멈춤.
             break
         }
-        else {
+        else { // 새로 들어온 프렛이 더 커지는 순간이 오면, 그 줄의 스택에 넣고 멈춤.
             stacks[line].append(fret)
             count += 1
             break
