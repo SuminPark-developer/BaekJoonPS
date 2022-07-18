d330e1e649e16349b3a7c8a608ab4c4a64e352b2
developer-p <float@kakao.com>
2022-07-18 22:32:53 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 12 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.swift
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.swift
@@ -1,4 +1,4 @@
-// MARK: - 1018번(완전탐색)
+// MARK: - 1018번(완전탐색 / 코드 개선)
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (N, M) = (input[0], input[1])
 
@@ -10,49 +10,29 @@ for _ in 0..<N {
 
 let (endY, endX) = ((N - 8) + 1, (M - 8) + 1)
 
-var answerArray: [Int] = []
+var answer: Int = Int.max
 for startY in 1...endY {
     for startX in 1...endX {
-        let answerTemp: Int = min(startW(startY, startX), startB(startY, startX)) // W 시작 Case, B 시작 Case 중 작은 값 저장.
-        answerArray.append(answerTemp)
+        let answerTemp: Int = min(fill(startY, startX, "W"), fill(startY, startX, "B")) // W 시작 Case, B 시작 Case 중 작은 값 저장.
+        answer = min(answer, answerTemp)
     }
 }
 
-print(answerArray.min()!) // 가장 최소값 출력.
+print(answer) // 가장 최소값 출력.
 
-func startW(_ startY: Int, _ startX: Int) -> Int { // W로 시작 Case.
+func fill(_ y: Int, _ x: Int, _ bw: String) -> Int {
     var count: Int = 0 // 변경 횟수 카운팅.
     
-    for y in startY..<startY + 8 {
-        for x in startX..<startX + 8 {
-            if ((y % 2 == 1) && (x % 2 == 1)) || ((y % 2 == 0) && (x % 2 == 0)) { // W여야 함.
-                if board[y][x] != "W" {
-                    count += 1
+    for i in 0..<8 {
+        for j in 0..<8 {
+            if (i + j) % 2 == 0 { // (홀, 홀) || (짝, 짝) -> bw와 같아야 함.
+                if board[y + i][x + j] != bw { // 같아야 하는데 다르면,
+                    count += 1 // 변경.
                 }
             }
-            else if ((y % 2 == 1) && (x % 2 == 0)) || ((y % 2 == 0) && (x % 2 == 1)) { // B여야 함.
-                if board[y][x] != "B" {
-                    count += 1
-                }
-            }
-        }
-    }
-    return count
-}
-
-func startB(_ startY: Int, _ startX: Int) -> Int { // B로 시작 Case.
-    var count: Int = 0 // 변경 횟수 카운팅.
-    
-    for y in startY..<startY + 8 {
-        for x in startX..<startX + 8 {
-            if ((y % 2 == 1) && (x % 2 == 1)) || ((y % 2 == 0) && (x % 2 == 0)) { // B여야 함.
-                if board[y][x] != "B" {
-                    count += 1
-                }
-            }
-            else if ((y % 2 == 1) && (x % 2 == 0)) || ((y % 2 == 0) && (x % 2 == 1)) { // W여야 함.
-                if board[y][x] != "W" {
-                    count += 1
+            else { // (홀, 짝) || (짝, 홀) -> bw와 달라야 함.
+                if board[y + i][x + j] == bw { // 달라야 하는데 같으면,
+                    count += 1 // 변경.
                 }
             }
         }
