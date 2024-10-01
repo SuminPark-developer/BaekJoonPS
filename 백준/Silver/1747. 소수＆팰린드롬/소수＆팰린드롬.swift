cabf8a4f78a4e8ad03fb3c429d4c89b903e2799f
developer-p <float@kakao.com>
2024-10-01 20:02:51 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 260 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.swift
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.swift
@@ -1,35 +1,37 @@
-// MARK: - 1747번(에라토스테네스의 체 + 팰린드롬)
+// MARK: - 1747번
 import Foundation
 var N = Int(readLine()!)!
+
 while true {
-    if isPrime(N) { // 소수인데,
-        if isPalindrome(String(N).map{String($0)}) { // 팰린드롬이기까지 하면,
-            print(N)
-            break
-        }
+    if isPrime(N) && isPalindrom(String(N).map{String($0)}) {
+        print(N)
+        break
     }
-    N += 1
+    else {
+        N += 1
+    }
 }
 
-func isPrime(_ num: Int) -> Bool { // 소수인지 확인하는 함수.
+func isPrime(_ num: Int) -> Bool {
     if num < 4 {
-        return num == 1 ? false : true // 1은 false, 2와 3은 true
+        return num == 1 ? false : true
     }
-    let end: Int = Int(sqrt(Double(num))) // Int(√n)까지만.
+    
+    let end = Int(sqrt(Double(num)))
     for i in 2...end {
-        if num % i == 0 { // 약수가 있다면, 소수가 아님.
+        if num % i == 0 {
             return false
         }
     }
     return true
 }
 
-func isPalindrome(_ numArray: [String]) -> Bool { // 팰린드롬인지 확인하는 함수.
-    let length: Int = numArray.count
+func isPalindrom(_ numArray: [String]) -> Bool {
+    let length = numArray.count
     var flag: Bool = true
-
+    
     for i in 0..<length/2 {
-        if numArray[i] != numArray[length - 1 - i] {
+        if numArray[i] != numArray[length - i - 1] {
             flag = false
             break
         }
