534fa4ecdf5eae0a5ae86f346db5bc0c2d429a86
developer-p <float@kakao.com>
2024-10-03 23:02:46 +0900

    [Silver I] Title: 미로 탐색, Time: 8 ms, Memory: 69256 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.swift
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.swift
@@ -1,88 +1,83 @@
-// MARK: - 2178번
-class Dequeue<T> {
-    var enQueue: [T]
-    var deQueue: [T] = []
+// MARK: - 2178번(BFS)
+class Deque<T> {
+    var enque: [T]
+    var deque: [T] = []
     
     var count: Int {
-        return enQueue.count + deQueue.count
+        return enque.count + deque.count
     }
     
     var isEmpty: Bool {
-        return enQueue.isEmpty && deQueue.isEmpty
+        return enque.isEmpty && deque.isEmpty
     }
     
     init(_ queue: [T]) {
-        enQueue = queue
-    }
-    
-    func pushLast(_ element: T) {
-        enQueue.append(element)
+        self.enque = queue
     }
     
     func pushFirst(_ element: T) {
-        deQueue.append(element)
+        deque.append(element)
     }
     
-    func popLast() -> T {
-        if enQueue.isEmpty {
-            enQueue = deQueue.reversed()
-            deQueue.removeAll()
-        }
-        return enQueue.popLast()!
+    func pushLast(_ element: T) {
+        enque.append(element)
     }
     
     func popFirst() -> T {
-        if deQueue.isEmpty {
-            deQueue = enQueue.reversed()
-            enQueue.removeAll()
+        if deque.isEmpty {
+            deque = enque.reversed()
+            enque.removeAll()
         }
-        return deQueue.popLast()!
+        return deque.popLast()!
     }
     
+    func popLast() -> T {
+        if enque.isEmpty {
+            enque = deque.reversed()
+            deque.removeAll()
+        }
+        return enque.popLast()!
+    }
 }
 
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (N, M) = (input[0], input[1])
-var adj: [[Int]] = []
 
+var board: [[Int]] = []
+board.append(Array(repeating: 0, count: M + 1))
+var visited = Array(repeating: Array(repeating: false, count: M + 1), count: N + 1)
 for _ in 0..<N {
     let input = readLine()!.map{Int(String($0))!}
-    adj.append(input)
+    board.append([0] + input)
+}
+
+func isValidCoord(_ y: Int, _ x: Int) -> Bool {
+    return (1 <= y && y <= N) && (1 <= x && x <= M)
 }
 
 let dy = [0, 1, 0, -1]
 let dx = [1, 0, -1, 0]
-var visited = Array(repeating: Array(repeating: false, count: M), count: N)
 
-func isValid(_ y: Int, _ x: Int) -> Bool {
-    return (0 <= y && y <= N - 1) && (0 <= x && x <= M - 1)
-}
-
-func bfs(_ startY: Int, _ startX: Int) -> Int {
-    visited[startY][startX] = true // 시작위치 방문 포함.
-    let q = Dequeue([(startY, startX, 1)])
+//var count: Int = 0
+func bfs(_ startY: Int, _ startX: Int) {
+    let q = Deque([(startY, startX, 1)])
     
     while !q.isEmpty {
-        let (y, x, d) = q.popFirst()
-        
-        if y == N-1 && x == M-1 { // 끝에 도달했으면,
-            return d
+        let (y, x, count) = q.popFirst()
+        if y == N && x == M { // 종료 조건
+            print(count)
+//            break
         }
         
         for k in 0..<4 {
             let ny = y + dy[k]
             let nx = x + dx[k]
-            let nd = d + 1
-            if isValid(ny, nx) && !visited[ny][nx] && adj[ny][nx] == 1 { // 정상 좌표고, 방문한 적 없고, 길이 갈 수 있으면(1이면),
+            if isValidCoord(ny, nx) && board[ny][nx] == 1 && !visited[ny][nx] { // 유효 범위이고, 연결되어 있는 길이고, 미방문 상태이면
                 visited[ny][nx] = true
-                q.pushLast((ny, nx, nd))
+                q.pushLast((ny, nx, count + 1))
             }
         }
     }
-    
-    return 1
-    
 }
 
-let answer = bfs(0, 0)
-print(answer)
+bfs(1, 1)
