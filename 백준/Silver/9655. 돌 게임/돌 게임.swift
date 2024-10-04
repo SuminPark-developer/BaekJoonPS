08b13eaefecf419cb544b966b9af2257012841b5
developer-p <float@kakao.com>
2024-10-04 18:30:24 +0900

    [Silver V] Title: 돌 게임, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.swift
+++ 백준/Silver/9655. 돌 게임/돌 게임.swift
@@ -1,8 +1,19 @@
 // MARK: - 9655번
 let N = Int(readLine()!)!
-if N % 2 == 1 {
+var cache = Array(repeating: -1, count: 1001) // 게임한 횟수
+cache[1] = 1
+cache[2] = 2
+cache[3] = 1
+
+if N >= 4 {
+    for i in 4...N {
+        cache[i] = min(cache[i - 1], cache[i - 3]) + 1
+    }
+}
+
+if cache[N] % 2 == 1 { // 게임한 횟수가 홀수면 '상근'
     print("SK")
 }
-else {
+else { // 짝수면 '창영'
     print("CY")
 }
