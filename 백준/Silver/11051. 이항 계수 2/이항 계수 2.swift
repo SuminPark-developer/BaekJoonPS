e25fd84c1eb19bf3b71c6ef0c285a86fc00aba3a
developer-p <float@kakao.com>
2024-10-04 17:26:54 +0900

    [Silver II] Title: 이항 계수 2, Time: 20 ms, Memory: 74952 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.swift
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.swift
@@ -1,4 +1,4 @@
-// MARK: - 11051번(DP - 재귀함수)
+// MARK: - 11051번(TopDown)
 let input = readLine()!.split(separator: " ").map{Int(String($0))!}
 let (N, K) = (input[0], input[1])
 var cache = Array(repeating: Array(repeating: -1, count: K + 1), count: N + 1)
@@ -7,16 +7,13 @@ func bino(_ n: Int, _ k: Int) -> Int {
     if cache[n][k] != -1 {
         return cache[n][k]
     }
-    
     if k == 0 || k == n {
         cache[n][k] = 1
     }
     else {
-        cache[n][k] = (bino(n - 1, k - 1) + bino(n - 1, k)) % 10007 // 나중에 answer에서 mod를 해주면 안됨.(그러면 Int범위 초과)
+        cache[n][k] = (bino(n - 1, k - 1) + bino(n - 1, k)) % 10007
     }
-    
     return cache[n][k]
 }
-
 let answer = bino(N, K)
 print(answer)
