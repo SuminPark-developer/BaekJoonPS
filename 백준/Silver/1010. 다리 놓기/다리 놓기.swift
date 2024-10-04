b9c8eb8f0ae70af3767bd530b941f781d0afed21
developer-p <float@kakao.com>
2024-10-04 17:41:40 +0900

    [Silver V] Title: 다리 놓기, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.swift
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.swift
@@ -1,25 +1,20 @@
-// MARK: - 1010번(DP - topDown)
+// MARK: - 1010번(BottomUp)
 let T = Int(readLine()!)!
+var cache = Array(repeating: Array(repeating: -1, count: 30), count: 30)
+
+for j in 1..<30 {
+    for i in 0...j {
+        if i == 0 || i == j {
+            cache[j][i] = 1
+        }
+        else {
+            cache[j][i] = cache[j - 1][i - 1] + cache[j - 1][i]
+        }
+    }
+}
 
 for _ in 0..<T {
     let input = readLine()!.split(separator: " ").map{Int(String($0))!}
     let (N, M) = (input[0], input[1])
-    
-    var cache = Array(repeating: Array(repeating: -1, count: 30), count: 30)
-    func bino(_ m: Int, _ n: Int) -> Int { // 이항계수
-        if cache[m][n] != -1 {
-            return cache[m][n]
-        }
-        
-        if n == 0 || n == m { // mC0 이나 mCm일 때,
-            cache[m][n] = 1
-        }
-        else { // n이 1 ~ m-1일 땐,
-            cache[m][n] = bino(m - 1, n - 1) + bino(m - 1, n)
-        }
-        
-        return cache[m][n]
-    }
-    let answer = bino(M, N)
-    print(answer)
+    print(cache[M][N]) // mCn
 }
