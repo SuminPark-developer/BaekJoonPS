743b81ec5f25fddc5a4f7576188ca9b32af8a817
developer-p <float@kakao.com>
2024-11-10 23:11:59 +0900

    [Bronze III] Title: 경고, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/3029. 경고/경고.swift
+++ 백준/Bronze/3029. 경고/경고.swift
@@ -1,27 +1,18 @@
 // MARK: - 3029번
 import Foundation
-let timeNow = readLine()!.split(separator: ":").map{Int(String($0))!} // 현재 시간
-let (nowH, nowM, nowS) = (timeNow[0], timeNow[1], timeNow[2])
-let timeThrow = readLine()!.split(separator: ":").map{Int(String($0))!} // 던질 시간
-var (throwH, throwM, throwS) = (timeThrow[0], timeThrow[1], timeThrow[2])
+let currentTime = readLine()!.split(separator: ":").map{Int(String($0))!}
+let throwTime = readLine()!.split(separator: ":").map{Int(String($0))!}
 
-var totalTime: Int = 0
+let currentSec: Int = currentTime[0] * 60 * 60 + currentTime[1] * 60 + currentTime[2]
+var throwSec: Int = throwTime[0] * 60 * 60 + throwTime[1] * 60 + throwTime[2]
 
-if (throwH == nowH) && (throwM == nowM) && (throwS == nowS) { // 시각이 같다면, 최대 24시간 기다림.
-    print("24:00:00")
+if currentSec >= throwSec { // 다음날 인경우(ex. 00:00:00도 대응해야됨.)
+    throwSec += 24 * 60 * 60
 }
-else {
-    
-    if throwH < nowH {
-        throwH += 24
-    }
-    
-    totalTime = ((throwH - nowH) * 3600) + ((throwM - nowM) * 60) + (throwS - nowS)
-    
-    let answerH = totalTime / 3600
-    let answerM = (totalTime % 3600) / 60
-    let answerS = (totalTime % 3600) % 60
-    
-    print("\(String(format: "%02d", answerH)):\(String(format: "%02d", answerM)):\(String(format: "%02d", answerS))")
-    
-}
+
+let answerSec: Int = throwSec - currentSec
+let answerHH = String(format: "%02d", answerSec / 3600)
+let answerMM = String(format: "%02d", answerSec % 3600 / 60)
+let answerSS = String(format: "%02d", answerSec % 3600 % 60)
+
+print("\(answerHH):\(answerMM):\(answerSS)")
