d709acc3b42b37344b14acc144f28fb4f8dd90af
developer-p <float@kakao.com>
2025-01-11 20:49:41 +0900

    [Bronze II] Title: 소수, Time: 8 ms, Memory: 79676 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.swift
+++ 백준/Bronze/2581. 소수/소수.swift
@@ -1,36 +1,35 @@
 // MARK: - 2581번
 import Foundation
+
 let M = Int(readLine()!)!
 let N = Int(readLine()!)!
 
-var isPrimeArray = [0] + Array(repeating: 0, count: N)
-if N == 1 { // 입력값(최대)이 1이면 바로 종료.
+var isPrimary = [0] + Array(repeating: 0, count: N)
+
+if N == 1 {
     print(-1)
     exit(0)
 }
 
 for i in 2...N {
-    isPrimeArray[i] = i
+    isPrimary[i] = i
 }
 
 for i in 2...N {
-    if isPrimeArray[i] == 0 {
+    if isPrimary[i] == 0 {
         continue
     }
     for j in stride(from: i*2, through: N, by: i) {
-        isPrimeArray[j] = 0
+        isPrimary[j] = 0
     }
 }
 
-isPrimeArray = Array(isPrimeArray[M...N])
-//print(isPrimeArray)
-
-let sum = isPrimeArray.filter{$0 != 0}.reduce(0, +)
+let sum = Array(isPrimary[M...N]).filter{$0 != 0}.reduce(0, +)
 if sum == 0 {
     print(-1)
 }
 else {
     print(sum)
-    let min = isPrimeArray.filter{$0 != 0}.first!
+    let min = Array(isPrimary[M...N]).filter{$0 != 0}.first!
     print(min)
 }
