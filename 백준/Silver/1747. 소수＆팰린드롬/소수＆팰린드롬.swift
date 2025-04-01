f6cd42806dc52b72348030e0ec8513b8b80a6ad4
developer-p <float@kakao.com>
2025-04-01 23:24:25 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 220 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.swift
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.swift
@@ -2,23 +2,11 @@
 import Foundation
 var N = Int(readLine()!)!
 
-while true {
-    if isPrime(N) && isPalindrom(String(N).map{String($0)}) {
-        print(N)
-        break
-    }
-    else {
-        N += 1
-    }
-}
-
 func isPrime(_ num: Int) -> Bool {
     if num < 4 {
         return num == 1 ? false : true
     }
-    
-    let end = Int(sqrt(Double(num)))
-    for i in 2...end {
+    for i in 2..<Int(sqrt(Double(num)) + 1) {
         if num % i == 0 {
             return false
         }
@@ -26,15 +14,24 @@ func isPrime(_ num: Int) -> Bool {
     return true
 }
 
-func isPalindrom(_ numArray: [String]) -> Bool {
+func isPalindrom(_ num: Int) -> Bool {
+    let numArray = String(num).map{String($0)}
     let length = numArray.count
     var flag: Bool = true
     
-    for i in 0..<length/2 {
+    for i in 0..<numArray.count/2 {
         if numArray[i] != numArray[length - i - 1] {
             flag = false
-            break
+            return flag
         }
     }
     return flag
+}
+
+while true {
+    if isPrime(N) && isPalindrom(N) {
+        print(N)
+        break
+    }
+    N = N + 1
 }
